38cd30192850d0725ed99c7f4b9204cfc1f47a4c
julio09 <jimmyjulio100@gmail.com>
2023-05-13 12:16:51 +0300

    create faker data with Faker package + controller

--- config/bundles.php
+++ config/bundles.php
@@ -12,5 +12,4 @@ return [
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
-    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
 ];
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,13 +2,26 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Client;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+
+use Faker\Factory;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
+
+        $faker = Factory::create('fr_FR');
+
+        for($i =0; $i < 5; $i++){
+            $clients = new Client();
+            $clients->setNumerodeCompte($faker->randomNumber)
+                    ->setNom($faker->name)
+                    ->setSolde(mt_rand(200, 1000));
+            $manager->persist($clients);
+        }
         // $product = new Product();
         // $manager->persist($product);
 
